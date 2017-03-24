15cf4ca01acb81775b575587c9c021b03f52b239
ASUS <ASUS@ASUS-PCKS>
2017-03-24 15:48:46 +0800

    layer

---
+++ eSaleDao/OrderDao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSaleDao
+{
+    public class OrderDao
+    {
+        /// <summary>
+        /// 依訂單編號取得訂單資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public eSaleModel.Order GetOrderById(String id) {
+            return new eSaleModel.Order()
+            { CustId = "KUAS", CustName = "高應大", OrderId = "1" };
+
+        }
+    }
+}
